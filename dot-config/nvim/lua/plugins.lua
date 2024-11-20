40000f4d0bbb5315ab3ef0b23d58deddc4dd1f35
QU4SIMOTO <jack@ifrit.localdomain>
2024-11-20 16:31:29 +0000

    Some hyprland bindings

--- dot-config/nvim/lua/plugins.lua
+++ dot-config/nvim/lua/plugins.lua
@@ -342,4 +342,10 @@ require("lazy").setup({
       vim.g.vim_markdown_auto_insert_bullets = 0
     end
   },
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+  }
 })
