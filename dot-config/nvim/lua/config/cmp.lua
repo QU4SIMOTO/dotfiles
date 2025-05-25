e26d579677c498edadd5de9fdb93a8f014f443f5
QU4SIMOTO <jackdixon247@outlook.com>
2025-05-25 13:46:52 +0100

    nvim-cmp changes and dadbod setup.

---
+++ dot-config/nvim/lua/config/cmp.lua
@@ -0,0 +1,67 @@
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.shortmess:append "c"
+
+
+local cmp = require "cmp"
+
+cmp.setup {
+  sources = {
+    {
+      name = "lazydev",
+      -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+      group_index = 0,
+    },
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "buffer" },
+  },
+  mapping = {
+    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+    ["<C-y>"] = cmp.mapping(
+      cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      },
+      { "i", "c" }
+    ),
+  },
+
+  formatting = {
+    fields = { "abbr", "kind", "menu" },
+    expandable_indicator = true,
+  },
+
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      -- Below is the default comparitor list and order for nvim-cmp
+      cmp.config.compare.offset,
+      -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.locality,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
+  },
+}
+
+-- Setup up vim-dadbod
+cmp.setup.filetype({ "sql" }, {
+  sources = {
+    { name = "vim-dadbod-completion" },
+    { name = "buffer" },
+  },
+})
+
+
+-- Enable completing paths in :
+cmp.setup.cmdline(":", {
+  sources = cmp.config.sources({
+    { name = "path" },
+  })
+})
--- dot-config/nvim/lua/config/plugins/nvim-cmp.lua
+++ dot-config/nvim/lua/config/plugins/nvim-cmp.lua
@@ -1,40 +1,19 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    -- load cmp on InsertEnter
-    event = { "InsertEnter", "CmdlineEnter", },
+    lazy = false,
+    priority = 100,
     dependencies = {
-      "neovim/nvim-lspconfig",
+      "onsails/lspkind.nvim",
       "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
-      'hrsh7th/vim-vsnip',
+      "hrsh7th/cmp-buffer",
+      { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+      "saadparwaiz1/cmp_luasnip",
+      "roobert/tailwindcss-colorizer-cmp.nvim",
     },
     config = function()
-      local cmp = require "cmp"
-
-      cmp.setup({
-        snippet = {
-          -- REQUIRED by nvim-cmp. get rid of it once we can
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end,
-        },
-        sources = cmp.config.sources(
-          { { name = "nvim_lsp" }, },
-          { { name = "path", }, }
-        ),
-        experimental = {
-          ghost_text = true,
-        },
-      })
-
-      -- Enable completing paths in :
-      cmp.setup.cmdline(":", {
-        sources = cmp.config.sources({
-          { name = "path" },
-        })
-      })
-    end
+      require "config.cmp"
+    end,
   },
 }
