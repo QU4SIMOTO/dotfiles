55bd20257518bbe4ef37855dbc103b2ef5cdd11b
QU4SIMOTO <jackdixon247@outlook.com>
2025-06-04 11:56:01 +0100

    Add json to neoformat file types.

--- dot-config/nvim/lua/config/plugins/neoformat.lua
+++ dot-config/nvim/lua/config/plugins/neoformat.lua
@@ -6,7 +6,7 @@ return {
       vim.g.neoformat_try_node_exe = 1
 
       vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = { "*.ts", "*.js", "*.tsx", },
+        pattern = { "*.ts", "*.js", "*.tsx", "*.json" },
         callback = function()
           vim.cmd("Neoformat")
         end
